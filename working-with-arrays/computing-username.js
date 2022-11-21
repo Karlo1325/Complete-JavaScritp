78d9af26decbac143197195145919aedc918a94c
“Karlo <cickokarlo@gmail.com>
2022-11-21 21:54:59 +0100

    creating a username function

---
+++ working-with-arrays/computing-username.js
@@ -0,0 +1,56 @@
+"use strict";
+// Computing username
+
+// Data
+const account1 = {
+  owner: "Krisitjan Salijević",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// ova funkcija uzme array sa objectima i u svakom objectu iz owner keya generira username
+const createUsername = function (accs) {
+  accounts.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+createUsername(accounts);
+console.log(account4.username);
+
+// primjer dio funkcije
+const user = "Steven Thomas Williams";
+const username = user
+  .toLowerCase()
+  .split(" ")
+  .map((name) => name[0])
+  .join("");
+
+console.log(username);
