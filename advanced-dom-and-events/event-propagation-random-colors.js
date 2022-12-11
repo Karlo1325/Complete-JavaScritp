a500b3fa9013b3b826e1907846d620765ceaa59e
“Karlo <cickokarlo@gmail.com>
2022-12-11 08:17:17 +0100

    examples

---
+++ advanced-dom-and-events/event-propagation-random-colors.js
@@ -0,0 +1,27 @@
+
+//
+// Event propagation in practice
+// creating random colors
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget === this); // shows where the click happend
+
+  // Stop propagation
+  //e.stopPropagation(); // stoping propagation, other elements dont change colors now(in practice we dont use it)
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
