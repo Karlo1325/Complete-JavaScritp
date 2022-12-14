92603c69ea29d5c9a379cb1c6c9674e0633e39a8
Karlo <cickokarlo@gmail.com>
2022-12-14 18:32:27 +0100

    loading high res images after low res for better performance

---
+++ advanced-dom-and-events/lazy-loading-img.js
@@ -0,0 +1,29 @@
+
+//
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]'); // selecting data-src images
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px', //delaying load img
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
